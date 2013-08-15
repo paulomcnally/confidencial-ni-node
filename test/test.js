4ddb48917771a6aad7b1152bede0d20596008a89
paulomcnally <paulomcnally@gmail.com>
2013-08-14 23:30:34 -0600

    0.0.5

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var confidencial = require('../lib/confidencial');
 
-confidencial.getArticle('http://www.confidencial.com.ni/articulo/13169/turismo-al-pie-del-mombacho', function(resultData) {
+confidencial.getArticle('http://www.confidencial.com.ni/articulo/13219/portazo-parlamentario-a-jarquin', function(resultData) {
     console.log(resultData);
 });
 
-confidencial.getAllLinks(function(data){
-   console.log(data);
-});
+//confidencial.getAllLinks(function(data){
+//   console.log(data);
+//});
